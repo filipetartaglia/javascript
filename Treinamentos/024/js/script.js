d4fa04e2eaa7abd0891f29ad47220c82c84f2693
Filipe Tartaglia <lipetartaglia9@gmail.com>
2024-08-29 11:13:57 -0300

    Avançando os estudos em JavaScript

---
+++ Treinamentos/024/js/script.js
@@ -0,0 +1,19 @@
+var x = 1;
+var y = 3; 
+
+console.log(x, y);
+
+function teste() {
+    var z = 0;
+    console.log(z);
+    console.log(x);
+}
+
+teste();
+
+if(true) {
+    var p = 1;
+
+}
+
+console.log(p);
