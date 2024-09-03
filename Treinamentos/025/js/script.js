49d0d5877e835125c3cdea3d39f1ef08e4911504
Filipe Tartaglia <lipetartaglia9@gmail.com>
2024-09-03 10:13:51 -0300

    Treinando algumas funções

---
+++ Treinamentos/025/js/script.js
@@ -0,0 +1,26 @@
+// parseFloat 10.0
+
+console.log(parseFloat('12.999'))
+console.log(Number.parseFloat('12.999'));
+
+// parseInt
+
+console.log(parseInt('10'));
+console.log(parseInt(16.96));
+
+// toFixed
+
+console.log(23.23515155165.toFixed(1));
+
+// isNaN
+
+console.log(isNaN('Teste'));
+console.log(isNaN(12));
+console.log(isNaN('14'));
+
+// MAX_VALUE e MIN_VALUE
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+console.log(2.23243453543534e+408);
