bcd288d35be32b94a4a13e99ade428e9813662e3
Filipe Tartaglia <lipetartaglia9@gmail.com>
2025-03-17 21:51:10 -0300

    Desafio dia 2

---
+++ 7DaysCode.js/Semana1/dia2.js
@@ -0,0 +1,29 @@
+const readline = require('readline');
+
+//let nomePessoa = ('Qual o seu nome?');
+//let idadePessoa =('Qual sua idade?');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Qual é o seu nome? ', (nome) => {
+
+    rl.question('Qual a sua idade? ', (idade) => {
+        
+        rl.question('Qual linguagem de programação você está estudando? ', (linguagemProg) => {
+            
+            rl.question(`Seu nome é ${nome}, você tem ${idade} anos, e esta estudando ${linguagemProg}. Correto? `, (corretoOuNao) => {
+
+                if (corretoOuNao == 'sim' || 'Sim' || 'Correto' || 'correto') {
+                    console.log('Que bom, vamos seguir em frente!');
+                } else {
+                    console.log('Por favor, revise as informações!');
+                }
+                
+            });
+            
+        });
+    });
+});
