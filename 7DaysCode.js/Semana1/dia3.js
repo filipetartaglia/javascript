053f299f388814e0a590d69dd2417f72018fb4f2
Filipe Tartaglia <lipetartaglia9@gmail.com>
2025-03-19 22:34:08 -0300

    Desenvolvendo perguntas dia3

--- 7DaysCode.js/Semana1/dia3.js
+++ 7DaysCode.js/Semana1/dia3.js
@@ -14,10 +14,36 @@ rl.question('Escolha uma área para seguir na programação: Frontend ou Backend
 
             rl.question('Frontend, ótima escolha! Agora escolha: React ou Vue? ', (linguagemFront) => {
 
-                rl.question('Boa! Você quer se especializar na área escolhida ou seguir se desenvolvendo para se tornar Fullstack? (opcao1) ou (opcao2) ', (escolhaOpcao1) => {
+                const formalizaLinguagemFront = linguagemFront.trim().toLowerCase();
 
-                    rl.question('E por fim, quais são as tecnologias nas quais você gostaria de se especializar? ')
-                });
+                if (formalizaLinguagemFront == 'react') {
+
+                    rl.question('React, está em alta no mercado! Agora escolha: você quer se especializar na área escolhida ou seguir se desenvolvendo para se tornar Fullstack? opção(1) ou opção(2) ', (escolhaOpcao1) => {
+
+                        if (escolhaOpcao1 == '1'){
+
+                            rl.question('E por fim, quais são as tecnologias nas quais você gostaria de se especializar? ', (quaisTec1) => {
+                                console.log('top');
+                            });
+                        } else if (escolhaOpcao1 == '2') {
+                            rl.question('E por fim, quais são as tecnologias nas quais você gostaria de se especializar? ', (quaisTec1) => {
+                                console.log('top2');
+                            });
+                        }
+                    });
+                } else {
+
+                    rl.question('Vue, ótimo está crescendo bastante! Agora escolha: você quer se especializar na área escolhida ou seguir se desenvolvendo para se tornar Fullstack? opção(1) ou opção(2) ', (escolhaOpcao2) => {
+
+                        if (escolhaOpcao2 == '1') {
+                            rl.question('E por fim, quais são as tecnologias nas quais você gostaria de se especializar? ', (quaisTec2) => {
+                                console.log('show');
+                            });
+                        }
+                    });
+                }
+
+                
             });
             break;
         
@@ -27,14 +53,18 @@ rl.question('Escolha uma área para seguir na programação: Frontend ou Backend
 
                 rl.question('Boa! Você quer se especializar na área escolhida ou seguir se desenvolvendo para se tornar Fullstack? (opcao1) ou (opcao2) ', (escolhaOpcao2) => {
 
-                    rl.question('E por fim, quais são as tecnologias nas quais você gostaria de se especializar? ')
+                    rl.question('E por fim, quais são as tecnologias nas quais você gostaria de se especializar? ', (quaisTec2) => {
+
+                    });
+
                 });
             });
             break;
 
         default:
             console.log('Ops! Algum erro, vamos tentar novamente');
+            process.exit();
             break;
-    }
-
+            
+    };
 });
